64a240ce8049ce739c58ffb2a92c0cb1b00f0c2e
GeraldinJr <geraldo.junior0207@gmail.com>
2021-06-08 21:11:55 -0300

    Me segue, vai!

---
+++ Desafio/Me_segue_no_insta!.js
@@ -0,0 +1,11 @@
+function solucao(largura, altura) {
+    if(largura === altura){
+      console.log("QUADRADA");
+    }
+    else if(largura>altura){
+      console.log("PAISAGEM");
+    }
+    else if(largura<altura){
+      console.log("RETRATO");
+    }
+  }
